fe78337404c36f48ef0824ad1bb43de3bb4211bc
Verf <verf@protonmail.com>
2024-03-12 09:43:04 +0800

    add all standard treesitter highlight groups, close #6

--- lua/deepwhite/scheme.lua
+++ lua/deepwhite/scheme.lua
@@ -69,14 +69,14 @@ function M.get_groups(c)
 		Delimiter = { link = "Identifier" },
 		Statement = { fg = c.base0, bg = c.light_orange }, -- Conditional Repeat Label Operator Keyword Exception
 		Operator = { link = "Identifier" },
-		PreProc = { link = "Question" }, -- Include Define Macro PreCondit
+		PreProc = { link = "Keyword" }, -- Include Define Macro PreCondit
 		Type = { fg = c.pink }, -- StorageClass Structure Typedef
 		Special = { link = "SpecialKey" }, -- SpecialChar Tag Delimiter SpecialComment Debug
 		Function = { link = "Identifier" },
 		Underlined = { underline = true },
 		Ignore = { fg = c.base7 },
 		Error = { link = "ErrorMsg" },
-		Todo = { link = "WarningMsg" },
+		Todo = { fg = c.green },
 
 		-- Plugins
 		-- LSP
@@ -94,40 +94,112 @@ function M.get_groups(c)
 		RenameNormal = { link = "Normal" },
 
 		-- Treesittere
-		-- syntax
-		["@keyword.function"] = { bg = c.light_purple },
-		["@keyword.operator"] = { link = "Keyword" },
+		["@variable"] = { link = "Identifier" },
+		["@variable.builtin"] = { link = "Type" },
+		["@variable.parameter"] = { link = "Identifier" },
+		["@variable.member"] = { link = "Identifier" },
+
+		["@constant"] = { link = "Constant" },
+		["@constant.builtin"] = { link = "Type" },
+		["@constant.macro"] = { link = "Define" },
+
+		["@module"] = { link = "Include" },
+		["@module.builtin"] = { link = "Include" },
+		["@label"] = { link = "Label" },
+
+		["@string"] = { link = "String" },
+		["@string.documentation"] = { link = "String" },
+		["@string.regexp"] = { link = "String" },
+		["@string.escape"] = { link = "SpecialChar" },
+		["@string.special"] = { link = "SpecialChar" },
+		["@string.special.symbol"] = { link = "Identifier" },
+		["@string.special.path"] = { link = "Underlined" },
+		["@string.special.url"] = { link = "Underlined" },
+
+		["@character"] = { link = "Character" },
+		["@character.special"] = { link = "SpecialChar" },
+
+		["@boolean"] = { link = "Boolean" },
+		["@number"] = { link = "Number" },
+		["@number.float"] = { link = "Float" },
+
+		["@type"] = { link = "Type" },
+		["@type.builtin"] = { link = "Type" },
+		["@type.qualifier"] = { link = "Type" },
+		["@type.definition"] = { link = "Typedef" },
+
+		["@attribute"] = { link = "PreProc" },
+		["@property"] = { link = "Identifier" },
+
+		["@function"] = { link = "Function" },
+		["@function.builtin"] = { link = "Special" },
+		["@function.call"] = { link = "Special" },
+		["@function.macro"] = { link = "Macro" },
+
+		["@function.method"] = { link = "@function" },
+		["@function.method.call"] = { link = "@function.call" },
 
 		["@constructor"] = { link = "Identifier" },
+		["@operator"] = { link = "Operator" },
 
-		["@function.call"] = { link = "Special" },
-		["@function.builtin"] = { link = "Type" },
-		["@method.call"] = { link = "@function.call" },
+		["@keyword"] = { link = "Keyword" },
+		["@keyword.coroutine"] = { link = "Keyword" },
+		["@keyword.function"] = { bg = c.light_purple },
+		["@keyword.operator"] = { link = "Operator" },
+		["@keyword.import"] = { link = "Include" },
+		["@keyword.storage"] = { link = "Keyword" },
+		["@keyword.repeat"] = { link = "Repeat" },
+		["@keyword.return"] = { link = "Keyword" },
+		["@keyword.debug"] = { link = "Debug" },
+		["@keyword.exception"] = { link = "Exception" },
 
-		["@type.builtin"] = { link = "Type" },
-		["@constant.builtin"] = { link = "Type" },
-		["@variable.builtin"] = { link = "Type" },
+		["@keyword.conditional"] = { link = "Conditional" },
+		["@keyword.conditional.ternary"] = { link = "Conditional" },
+		["@keyword.directive"] = { link = "PreProc" },
+		["@keyword.directive.define"] = { link = "PreProc" },
 
-		["@text.strong"] = { bold = true },
-		["@text.emphasis"] = { italic = true },
-		["@text.strike"] = { strikethrough = true },
-		["@text.literal"] = { bg = c.light_orange },
-		["@text.literal.block"] = {},
-		["@text.title.gitcommit"] = { link = "@spell" },
-		["@text.quote"] = { fg = c.base1 },
-		["@text.reference"] = { underline = true },
+		["@punctutation.delimiter"] = { link = "Delimiter" },
+		["@punctutation.bracket"] = { link = "Delimiter" },
+		["@punctutation.special"] = { link = "Delimiter" },
 
-		-- adapt to https://github.com/nvim-treesitter/nvim-treesitter/pull/5895
-		["@function.method.call"] = { link = "@function.call" },
+		["@comment"] = { link = "Comment" },
+		["@comment.documentation"] = { link = "Comment" },
+
+		["@comment.warning"] = { link = "WarningMsg" },
+		["@comment.error"] = { link = "Error" },
+		["@comment.todo"] = { link = "Todo" },
+		["@comment.note"] = { link = "SpecialComment" },
 
 		["@markup.strong"] = { bold = true },
 		["@markup.emphasis"] = { italic = true },
+		["@markup.underline"] = { underline = true },
 		["@markup.strike"] = { strikethrough = true },
-		["@markup.raw"] = { bg = c.light_orange },
-		["@markup.raw.block"] = {},
+
+		["@markup.heading"] = { link = "Title" },
 		["@markup.heading.gitcommit"] = { link = "@spell" },
-		["@markup.quote"] = { fg = c.base1 },
-		["@markup.link"] = { underline = true },
+
+		["@markup.quote"] = { link = "Comment" },
+		["@markup.math"] = { link = "Special" },
+		["@markup.environment"] = { link = "Macro" },
+
+		["@markup.link"] = { link = "Underlined" },
+		["@markup.link.label"] = { link = "SpecialChar" },
+		["@markup.link.url"] = { link = "Keyword" },
+
+		["@markup.raw"] = { link = "SpecialComment" },
+		["@markup.raw.block"] = {},
+
+		["@markup.list"] = { link = "Identifier" },
+		["@markup.list.checked"] = { link = "Comment" },
+		["@markup.list.unchecked"] = { link = "Identifier" },
+
+		["@tag"] = { link = "Label" },
+		["@tag.delimiter"] = { link = "Delimiter" },
+		["@tag.attribute"] = { link = "Identifier" },
+
+		["@diff.plus"] = { link = "DiffAdd" },
+		["@diff.minus"] = { link = "DiffDelete" },
+		["@diff.delta"] = { link = "DiffChange" },
 
 		-- rainbow
 		TSRainbowRed = { fg = c.red },
