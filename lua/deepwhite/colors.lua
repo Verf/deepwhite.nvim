ccad56bb5eb7d29258980cfc0a716eb87bcad69e
d1gt <11901275+d1gt@users.noreply.github.com>
2024-09-30 05:42:10 +0300

    DapUI Colors (#11)
    * dapui colors
    
    ---------
    
    Co-authored-by: d1gt <l8@pm.me>

--- lua/deepwhite/colors.lua
+++ lua/deepwhite/colors.lua
@@ -28,6 +28,9 @@ function M.get_colors(options)
 		purple = "#6F00A6", -- hsv(280, 100%, 65%)
 		pink = "#A6006F", -- hsv(320, 100%, 65%)
 		red = "#A60000", -- hsv(360, 100%, 65%)
+
+		iris = '#907aa9',
+		muted = '#9893a5',
 	}
 	if options.low_blue_light then
 		colors.base7 = "#FAFAFA" -- hsv(60, 0%, 98%)
